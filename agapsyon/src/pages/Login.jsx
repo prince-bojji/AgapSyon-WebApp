afc0339293f5787cdcc1783f19d8e4a27106a5fe
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2023-07-22 22:32:20 +0800

    added landing, signup, login page, and other packages needed

---
+++ agapsyon/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+
+function Login() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    // Add form validation and submission
+  };
+
+  useEffect(() => {
+    document.body.style.background =
+      'linear-gradient(to right, #FFEDCC, #BFCFFF)';
+    document.body.style.height = '100vh';
+    return () => {
+      document.body.style.background = null;
+      document.body.style.height = null;
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col md:flex-row h-screen bg-['linear-gradient(to right, #FFEDCC, #BFCFFF)'] m-0 p-0">
+      {/* Left side */}
+      <div className='w-full md:w-1/2 h-full flex flex-col items-center p-10'>
+        <img
+          src='/src/images/logo.png'
+          alt='Logo'
+          className='h-[225px] w-[225px] m-0 mt-[20px] self-center'
+        />
+        <h1 className='text-2xl md:text-3xl font-bold m-0 mt-5 text-center'>
+          AgapSyon: Paghanda at Pag-aksyon sa mga Sakuna
+        </h1>
+      </div>
+
+      {/* Right side */}
+      <div className='w-full md:w-1/2 h-full bg-gray-200 flex flex-col items-center justify-center p-10'>
+        <form
+          onSubmit={onSubmit}
+          className='w-full space-y-4 flex flex-col items-center'>
+          <input
+            name='email'
+            type='email'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder='Email address'
+            className='w-2/3 p-2 border border-gray-200 rounded'
+          />
+          <input
+            name='password'
+            type='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder='Password'
+            className='w-2/3 p-2 border border-gray-200 rounded'
+          />
+          <div className='flex flex-col items-center mt-4'>
+            <button
+              type='submit'
+              className='w-half p-2 bg-green-500 text-white rounded mt-4'>
+              Login
+            </button>
+            <div className='mt-4 text-blue-500'>
+              <a href='/signup'>Don't have an account? Sign up</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
